99283fda68756bbd93fe813fe22a3ac99b8e1ece
Yongje Shin <yongje1211@gmail.com>
2023-02-14 09:57:25 +0900

    스프링 컨테이너로 통합

--- src/main/java/tobyspring/helloboot/HelloController.java
+++ src/main/java/tobyspring/helloboot/HelloController.java
@@ -1,8 +1,12 @@
 package tobyspring.helloboot;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import java.util.Objects;
 
-//@RestController
+@RequestMapping("/hello")
 public class HelloController {
 
     private final HelloService helloService;
@@ -11,6 +15,8 @@ public class HelloController {
         this.helloService = helloService;
     }
 
+    @GetMapping
+    @ResponseBody
     public String hello(String name) {
         return helloService.sayHello(Objects.requireNonNull(name));
     }
