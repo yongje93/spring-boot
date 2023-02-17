0a3096141cbaca65e94edf5d181b7f03cb81ad05
Yongje Shin <yongje1211@gmail.com>
2023-02-17 11:14:32 +0900

    Environment 추상화, 프로퍼티 @Value와 PropertySourcesPlaceholderConfigurer

--- src/test/java/tobyspring/helloboot/HelloApiTest.java
+++ src/test/java/tobyspring/helloboot/HelloApiTest.java
@@ -16,7 +16,7 @@ public class HelloApiTest {
         // http localhost:8080/hello?name=Spring
         TestRestTemplate rest = new TestRestTemplate();
 
-        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name={name}", String.class, "Spring");
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/app/hello?name={name}", String.class, "Spring");
 
         // status 200
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.OK);
@@ -30,7 +30,7 @@ public class HelloApiTest {
     void failHelloApi() {
         TestRestTemplate rest = new TestRestTemplate();
 
-        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/hello?name=", String.class);
+        ResponseEntity<String> res = rest.getForEntity("http://localhost:8080/app/hello?name=", String.class);
 
         // status 500
         assertThat(res.getStatusCode()).isEqualTo(HttpStatus.INTERNAL_SERVER_ERROR);
