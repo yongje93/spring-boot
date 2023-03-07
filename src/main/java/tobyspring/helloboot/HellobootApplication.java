9c06f35dcddb45b5513cfcf8100a1cfb5b4716b7
Yongje Shin <yongje1211@gmail.com>
2023-03-07 22:41:21 +0900

    spring boot 3.0.4로 update

--- src/main/java/tobyspring/helloboot/HellobootApplication.java
+++ src/main/java/tobyspring/helloboot/HellobootApplication.java
@@ -1,10 +1,9 @@
 package tobyspring.helloboot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jdbc.core.JdbcTemplate;
-
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class HellobootApplication {
